46c4da6679611609621a90b00ae317a65246821b
devmeist3r <devmeist3r@gmail.com>
2021-03-02 23:58:13 -0300

    *

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from 'react-icons/md';
 
 import * as Styled from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Aside: React.FC = () => {
   return (
     <Styled.Container>
-      <h1>Aside</h1>
+      <Styled.Header>
+        <Styled.LogImg src={logoImg} alt="Logo minha Carteira" />
+        <Styled.Title>Minha Carteira</Styled.Title>
+      </Styled.Header>
+
+      <Styled.MenuContainer>
+        <Styled.MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </Styled.MenuItemLink>
+        <Styled.MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </Styled.MenuItemLink>
+      </Styled.MenuContainer>
     </Styled.Container>
   );
 };
